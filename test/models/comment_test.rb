91011fd8d9daf3f4a8bc4bb75b4c2c0259484bd4
Rafael-RT <toizhanov77@gmail.com>
2021-12-21 18:32:43 +0600

    assignment 2

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,13 +3,23 @@
 # Table name: comments
 #
 #  id         :integer          not null, primary key
-#  post_id    :integer
 #  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  post_id    :integer          not null
+#  user_id    :integer
 #
-
-require 'test_helper'
+# Indexes
+#
+#  index_comments_on_post_id  (post_id)
+#  index_comments_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  post_id  (post_id => posts.id)
+#  user_id  (user_id => users.id)
+#
+require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
   # test "the truth" do
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -3,13 +3,21 @@
 # Table name: posts
 #
 #  id         :integer          not null, primary key
-#  title      :string
 #  body       :text
+#  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
-
-require 'test_helper'
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
+require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
   # test "the truth" do

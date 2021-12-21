de467aea091549ba8d955f0999b7be77a796519b
Rafael-RT <toizhanov77@gmail.com>
2021-12-21 18:21:43 +0600

    assignment 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912190510) do
+ActiveRecord::Schema.define(version: 20170321204630) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
